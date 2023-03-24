97105d2f9093b187f5ec892166eb1cb12d300283
Lazar <profkosmosic@gmail.com>
2023-03-24 16:11:40 +0100

    added comment bean

---
+++ src/java/beans/Comment.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author Lazar
+ */
+public class Comment {
+    private Integer commentId;
+    private String commentContent;
+    private Timestamp commentDate;
+    private Integer userId;
+    private Integer postId;
+
+    public Comment() {
+    }
+
+    public Comment(Integer commentId, String commentContent, Timestamp commentDate, Integer userId, Integer postId) {
+        this.commentId = commentId;
+        this.commentContent = commentContent;
+        this.commentDate = commentDate;
+        this.userId = userId;
+        this.postId = postId;
+    }
+
+    public Integer getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Integer commentId) {
+        this.commentId = commentId;
+    }
+
+    public String getCommentContent() {
+        return commentContent;
+    }
+
+    public void setCommentContent(String commentContent) {
+        this.commentContent = commentContent;
+    }
+
+    public Timestamp getCommentDate() {
+        return commentDate;
+    }
+
+    public void setCommentDate(Timestamp commentDate) {
+        this.commentDate = commentDate;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+}
